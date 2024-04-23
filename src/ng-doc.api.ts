81cc868577c605a0c11127215d87bdc367cc792b
Phillip Ninan <ninan.phillip@gmail.com>
2024-04-22 22:30:50 -0400

    fix: api feat is now working!

--- src/ng-doc.api.ts
+++ src/ng-doc.api.ts
@@ -6,9 +6,13 @@ const Api: NgDocApi = {
 		{
 			name: 'my-library-name',
 			route: 'my-library',
-			include: 'src/shared/**/*.ts',
+			include: './src/shared/**/*.ts',
 		  },		
 	],
 };
 
+// notes to update docs
+// 1. this file needs to be inside src
+//	 but the include is relative to the root
+// 
 export default Api;
--- src/shared/lui-button/lui-button.component.ts
+++ src/shared/lui-button/lui-button.component.ts
@@ -8,11 +8,23 @@ import { ChangeDetectionStrategy, Component, input } from "@angular/core";
         <button (click)="onClick()">{{buttonText()}}</button>
     `
 })
-export default class LuiButtonComponent{
+export class LuiButtonComponent{
     
+    /**
+     * some docs...
+     */
     buttonText = input<String>("Button");
     
+    /**
+     * some more docs...
+     */
     onClick(): void {
         console.log('Hello world');
       }    
+    /**
+     * some more docs...
+     */
+    onUnclick(): void {
+        console.log('Hello world');
+      }         
 }
